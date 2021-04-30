ad0d5628048e13df545cfcaa0fdfc3151bbfdd3d
Dimitri Gilbert <dimitri.gilbert@gmail.com>
2021-04-30 08:22:15 +0200

    new Utils class + setting improvements

--- src/Ability/Setting.php
+++ src/Ability/Setting.php
@@ -74,50 +74,90 @@ trait Setting {
     $name = $setting['inconsistant_wp_fix'];
     $display = '';
     if (isset($this->settings['settings'][$name])) {
-      // $display .= $this->settingField($this->settings['settings'][$name]);
-      $display = '<input
-          type="'.$this->settings['settings'][$name]['type'].'"
-          value="'.$this->getOption($name).'"
-          name="'.$name.'"
-          id="'.$name.'"
-        />';
+      $display .= $this->settingField($this->settings['settings'][$name]);
+      // $display = '<input
+      //     type="'.$this->settings['settings'][$name]['type'].'"
+      //     value="'.$this->getOption($name).'"
+      //     name="'.$name.'"
+      //     id="'.$name.'"
+      //   />';
     }
     echo $display;
   }
 
-  public function settingField($setting) {
+  public function settingField($setting, $value = null) {
     $display = '';
-    switch ($setting) {
+    switch ($setting['type']) {
       case 'object':
         break;
       case 'boolean':
-        $display = '<input
-          type="checkbox"
-          checked="'.$this->getOption($setting['name']).'"
-          name="'.$setting['name'].'"
-          id="'.$setting['name'].'"
-        />';
+        $display = $this->settingInputField(
+          $setting['name'],
+          !is_null($value)?$value:$this->getOption($setting['name']),
+          $setting['name'],
+          'checkbox'
+        );
         break;
       case 'array':
-        $display .= '<div>';
-        foreach ($this->getOption($setting['name']) as $setVal) {
-          $display .= '<div><input
-            type="'.$setting['type'].'"
-            value="'.$setVal.'"
-            name="'.$setting['name'].'[]"
-          /></div>';
-        }
-        $display .= '<button type="button" onclick="event.target.insertBefore(event.target.parentNode.firstElementChild.cloneNode())">Add One</button></div>';
+        $display = $this->settingArrayField(
+          $setting['name'],
+          !is_null($value)?$value:$this->getOption($setting['name'], [])
+        );
         break;
       default:
-        $display = '<input
-            type="'.$setting['type'].'"
-            value="'.$this->getOption($setting['name']).'"
-            name="'.$setting['name'].'"
-            id="'.$setting['name'].'"
-          />';
-        break;
+        $display = $this->settingInputField(
+          $setting['name'],
+          !is_null($value)?$value:$this->getOption($setting['name'])
+        );
     }
+    return $display;
+  }
+
+  public function settingInputField(string $name, $value = null, string $id = null, string $type = 'text'): string {
+    return '<input
+      type="'.$type.'"
+      value="'.$value.'"
+      name="'.$name.'"
+      id="'.(!is_null($id)?$id:$name).'"
+    />';
+  }
+
+  public function settingArrayField(string $name, $values = []) {
+    $display = '<div>';
+    $i = 0;
+    foreach ($values as $setVal) {
+      $display .= '<div>'.$this->settingInputField(
+        $name.'[]',
+        $setVal,
+        $name.'_'.$i
+      ).'</div>';
+      $i++;
+    }
+    $display .= '<button type="button" onclick="event.target.insertBefore(event.target.parentNode.firstElementChild.cloneNode())">Add One</button></div>';
+    return $display;
+  }
+
+  public function settingObjectField(string $name, $data = [], $specs = []) {
+    $display = '<div>';
+    foreach ($data as $key => $val) {
+      $type = 'text';
+      $fieldName = $name.'_'.$key;
+
+      if (isset($specs[$key])) {
+        if (isset($specs[$key]['type'])) {
+          $type = $specs[$key]['type'];
+        }
+        if (isset($specs[$key]['name'])) {
+          $fieldName = $specs[$key]['name'];
+        }
+      }
+
+      $display .= '<div id="'.$fieldName.'_container"><label>'.$key.'</label>'.$this->settingField([
+        'type'=>$type,
+        'name' => $fieldName
+      ], $val).'</div>';
+    }
+    $display .= '</div>';
     return $display;
   }
 
---
+++ src/Utils.php
@@ -0,0 +1,42 @@
+<?php
+namespace Dbuild\WpPlugin;
+
+class Utils
+{
+  /**
+   * Running doing_filter/action on a list, returns the first running.
+   *
+   * @param array $haystack
+   * @param string $type
+   * @return string|null
+   */
+  public static function DoingWhichHook(array $haystack, string $type) {
+    $fn = "doing_".$type;
+    foreach ($haystack as $needle) {
+      if (call_user_func_array($fn, [$needle])) {
+        return $needle;
+      }
+    }
+    return null;
+  }
+
+  /**
+   * Running doing_filter on a list, returns the first running.
+   *
+   * @param array $haystack
+   * @return string|null
+   */
+  public static function DoingWhichFilter(array $haystack) {
+    return self::DoingWhichHook($haystack, 'filter');
+  }
+
+  /**
+   * Running doing_action on a list, returns the first running.
+   *
+   * @param array $haystack
+   * @return string|null
+   */
+  public static function DoingWhichAction(array $haystack) {
+    return self::DoingWhichHook($haystack, 'action');
+  }
+}
